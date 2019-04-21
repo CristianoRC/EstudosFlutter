d508b4d20a6703cb8f33f47b75979e4471dd3b03
Cristiano Cunha <contato@cristianoprogramador.com>
2019-04-21 09:53:00 -0300

    Correções nos testes

--- Testes/test/teste_Descontos.dart
+++ Testes/test/teste_Descontos.dart
@@ -4,17 +4,19 @@ import '../descontos.dart';
 void main() {
   const valorSemDesconto = 150.0;
 
-  test('Deve clacular desconto corretamente', () {
-    var desconto = 25.0;
-    expect(calcularDesconto(valorSemDesconto, desconto, false),
-        valorSemDesconto - desconto);
+  test('Deve clacular desconto corretamente utilizando valores decimais', () {
+    const desconto = 25.0;
+    const valorComDesconto = valorSemDesconto - desconto;
+
+    expect(
+        calcularDesconto(valorSemDesconto, desconto, false), valorComDesconto);
   });
 
-  test('Deve clacular desconto(Porcentagem) corretamente', () {
+  test('Deve clacular desconto corretamente utilizando porcentagem', () {
     var desconto = 10.0;
-    var valorCalculadoManualmente = 135.0;
-    expect(calcularDesconto(valorSemDesconto, desconto, true),
-        valorCalculadoManualmente);
+    var valorComDesconto = 135.0;
+    expect(
+        calcularDesconto(valorSemDesconto, desconto, true), valorComDesconto);
   });
 
   test('Deve dar erro ao calcula valor com desconto negativo ou zero', () {
@@ -25,12 +27,12 @@ void main() {
         throwsA(TypeMatcher<ArgumentError>()));
   });
 
-  test('Deve dar erro ao calcula valor com valor inicial negativo ou zero', () {
+  test('Deve dar erro ao calcula valor com valor inicial zero', () {
     expect(() => calcularDesconto(0, 15, false),
         throwsA(TypeMatcher<ArgumentError>()));
   });
 
-  test('Deve dar erro ao calcula valor com valor inicial negativo ou zero', () {
+  test('Deve dar erro ao calcula valor com valor inicial negativo', () {
     expect(() => calcularDesconto(-1, 15, false),
         throwsA(TypeMatcher<ArgumentError>()));
   });
